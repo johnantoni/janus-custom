d1edee32a844a6536dc0f0a9090e535bd2263d1e
John Griffiths <john@johnantoni.com>
2012-08-24 13:22:03 -0400

    update task

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 task :default do
-  # update plugins
-  system("git submodule -q foreach git pull -q origin master")
-
   # symlink files
   system("ln -s ~/.janus/.ackrc ~/.ackrc")
   system("ln -s ~/.janus/.vimrc.before ~/.vimrc.before")
   system("ln -s ~/.janus/.vimrc.after ~/.vimrc.after")
+end
+
+task :update do
+  # update plugins
+  system("git submodule -q foreach git pull -q origin master")
 end
